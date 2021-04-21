db64edde526187fe06f3270123b651d70fb11460
Home <Home@LAPTOP-2E6V6ACM>
2021-04-21 20:56:33 +0300

    added dialer regions

--- KeyPadMVVM/ViewModels/DialingPageViewModel.cs
+++ KeyPadMVVM/ViewModels/DialingPageViewModel.cs
@@ -8,21 +8,31 @@ namespace KeyPadMVVM.ViewModels
 {
     class DialingPageViewModel:ViewModelBase
     {
-       
+        #region Commands
         public ICommand CancelCommand { get; set; }
+        #endregion
+
+        #region Constructor
         public DialingPageViewModel()
         {
             CancelCommand = new Command(async () => { await Application.Current.MainPage.Navigation.PopModalAsync(); });
         }
-       
+        #endregion
+
+        #region Properties
+
         private string dialNumber = "";
-        public string DialNumber { get => dialNumber;
+        public string DialNumber
+        { get => dialNumber;
             set { 
                 if(dialNumber!=value)
                 {
                     dialNumber = value;
                     OnPropertyChange(nameof(DialNumber));
                 }
-                    } }
+                    
+            } 
+        }
+        #endregion
     }
 }
